047d2f463ffdb2407cb297212dd13bf1fa5c15eb
cutephomaique12 <dmaccphu@gmail.com>
2026-09-24 16:23:04 +0700

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,1 +1,5 @@
+import { createClient } from "@supabase/supabase-js";
 
+export const db = createClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!, {
+  auth: { persistSession: false },
+});
